6e99e59257bc836cfa4817d224e23dc39aae7f08
Pradhan, Debasish (US - Bengaluru) <debpradhan@deloitte.com>
2017-04-10 20:33:41 +0530

    Login UI redesigned

--- app/src/main/java/com/fi/botapp/ui/LoginActivity.java
+++ app/src/main/java/com/fi/botapp/ui/LoginActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -70,7 +71,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.emailid_edittext);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
